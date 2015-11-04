cef7ecfd83612f8cac4560034b3e01988f27c7ec
Igor Pashev <pashev.igor@gmail.com>
2015-11-04 15:38:53 +0300

    Do not reduce entropy

--- src/Web/ZeroBin/Utils.hs
+++ src/Web/ZeroBin/Utils.hs
@@ -7,13 +7,11 @@ import Crypto.Random.Entropy (getEntropy)
 import Data.ByteString (ByteString)
 import Data.ByteString.Base64 (encode)
 import Data.ByteString.Char8 (unpack)
-import Data.Char (isAlphaNum)
 
 
 toWeb :: ByteString -> String
 toWeb = takeWhile (/= '=') . unpack . encode
 
 makePassword :: Int -> IO String
-makePassword n = (map (\c -> if isAlphaNum c then c else 'X')
-                  . toWeb) `fmap` getEntropy n
+makePassword n = toWeb `fmap` getEntropy n
 
